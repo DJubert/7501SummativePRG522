c224b69ead493355c5c4cc5940ca42e747ce2b74
DJubert <joubert.dean2001@gmail.com>
2021-09-22 13:53:18 +0200

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SA7810PRG522_WebApp.Controllers
+
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+
+
+            return View();
+        }
+
+        public ActionResult Services()
+        {
+
+            return View();
+        }
+        [Authorize]
+        public ActionResult Portfolio()
+        {
+
+
+            return View();
+        }
+    }
+}
